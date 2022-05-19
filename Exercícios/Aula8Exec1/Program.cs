5b1ec9a5d979632011d95d228a57492422e59d0b
Rafael Suarez <rafaelv_s@hotmail.com>
2022-05-19 16:31:20 -0300

    Aula 8 - Exec1 e Exec2

---
+++ Exercícios/Aula8Exec1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula8Exec1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            // leitura de N
+            N = int.Parse(Console.ReadLine());
+            // leitura de valores em formato texto
+            string[] linha = Console.ReadLine().Split();
+            // declaração de vetor e criação de instância
+            double[] valores = new double[N];
+
+            double media, soma = 0.0;
+            
+            // percorre vetor texto e atribui valores
+            for(int i=0;i<N;i++){
+                valores[i] = double.Parse(linha[i],CultureInfo.InvariantCulture);
+                soma += valores[i];
+            }
+            
+            media = soma / N;
+
+            for(int i=0; i<N; i++ ){
+                Console.Write(valores[i].ToString("F1",CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(soma.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
---
+++ Exercícios/Aula8Exec2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula8Exec2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+            Faça um programa que leia N números reais e armazene-os em um vetor.
+            Em seguida, mostrar na tela o maior número do vetor (supor não haver
+            empates). Mostrar também a posição do maior elemento.
+            6
+            8.0 4.0 10.0 14.0 13.0 7.0
+            14.0
+            3
+            */
+            int N = int.Parse(Console.ReadLine());
+            string[] linha = Console.ReadLine().Split();
+            double[] valores = new double[N];
+            double maior = 0.0;
+            int posicao = 0;
+            // percorre o vetor da linha
+            for(int i=0;i<N ; i++){
+                // atribui o valor de cada posicao do vetor linha
+                valores[i] = double.Parse(linha[i],CultureInfo.InvariantCulture);
+            }
+
+            // percore o vetor de valores double
+            for(int i=0; i < N ; i++){
+                // se vetor na posicao i for maior que a var maior
+                if(valores[i] > maior){
+                    // atribui o valor da var maior ao valores na posicao i
+                    maior = valores[i];
+                    // atribui o valor da var posicao ao valor do indice i
+                    posicao = i;
+                }    
+            }
+            Console.WriteLine(maior.ToString("F1",CultureInfo.InvariantCulture));
+            Console.WriteLine(posicao);
+            
+        }
+    }
+}
